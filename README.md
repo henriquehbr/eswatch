3610f5d2e18407e779e62e6a8c4e670d61919844
Henrique Borges <henriqueborgeshbr@gmail.com>
2020-10-08 19:31:24 -0300

    chore: upload files

--- README.md
+++ README.md
@@ -1,19 +1,5 @@
-# ts-node-boilerplate
+# esbuild-watch
 
-A fully-structured project structure for writing Node with TypeScript
+A watcher utility for esbuild
 
-## Start
-
-In order to get started, just run `yarn` (or npm) to install all the required dependencies to run your application (All the core dependencies are `devDependencies`, avoiding unnecessary code and keeping your bundle size as small as possible)
-
-## Development
-
-For a quick and lean development experience, you can run `yarn dev` for a lightning-fast reloading utility to preview the changes on your application almost instantly
-
-## Build
-
-Run `yarn build` to lint, transpile, and minify your entire application to production-ready vanilla JavaScript
-
-## Lint
-
-In order to check if your code is properly optimized, formatted and matches ESLint rules, run `yarn lint`, it will scan all files with a `.ts` extension on `src`, and also, will run the TypeScript Compiler (`tsc`) to check for inconsistencies on your code
+> This project is in very early development stage, it's not advised to use it in production, at the moment, it's only a proof-of-concept
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,48 @@
-import inquirer from 'inquirer'
+import readline from 'readline'
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
+import { ChildProcess, fork } from 'child_process'
+import minimist from 'minimist'
+import esbuild from 'esbuild'
+import chokidar from 'chokidar'
 
-console.log('Starting subprocess...')
+interface LiveReloadOptions {
+  watch: string | readonly string[]
+  clear: boolean
+  entry: string
+}
 
-inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'username',
-      default: 'World'
-    }
-  ])
-  .then(answers => {
-    console.log(`Hello ${answers.username}!`)
+const __dirname = dirname(fileURLToPath(import.meta.url))
+const argv = minimist<LiveReloadOptions>(process.argv.slice(2))
+
+let child: ChildProcess
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+const watchAndBuild = async (options: LiveReloadOptions) => {
+  const outdir = path.join(__dirname, 'build')
+  child && child.kill()
+  const service = await esbuild.startService()
+  options.clear && console.clear()
+  // TODO: switch to yargs later, as minimist doesn't support array options
+  await service.build({
+    entryPoints: [options.entry],
+    outdir
   })
+  return new Promise<void>(resolve => {
+    rl.pause()
+    child = fork(outdir, { stdio: 'inherit' })
+    child.on('close', () => {
+      rl.resume()
+      resolve()
+    })
+  })
+}
+
+if (!argv.watch) throw new Error('Missing "watch" argument')
+const watcher = chokidar.watch(argv.watch)
+watcher.on('ready', async () => watchAndBuild(argv))
+watcher.on('change', async () => watchAndBuild(argv))
