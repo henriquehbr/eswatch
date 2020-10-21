0254bc9ac6cea5e338f55448d65576420c03594b
Henrique Borges <henriqueborgeshbr@gmail.com>
2020-10-21 17:54:41 -0300

    feat: allow entry point to be passed without flag

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,26 @@ import { fileURLToPath } from 'url'
 import { ChildProcess, spawn } from 'child_process'
 import minimist from 'minimist'
 import esbuild from 'esbuild'
+import { existsSync } from 'fs'
 
 interface LiveReloadOptions {
   watch: string | readonly string[]
-  clear: boolean
+  clear?: boolean
   entry: string
-  outdir: string
-  run: boolean
+  outdir?: string
+  run?: boolean
 }
 
 const __dirname = dirname(fileURLToPath(import.meta.url))
 const options = minimist<LiveReloadOptions>(process.argv.slice(2))
+
+// TODO: put entry point validation on a separated util module
+const missingEntryPoint = options._.length < 1 && !options.entry
+const invalidEntryPoint = !options._.every(entryPoint => existsSync(entryPoint))
+if (missingEntryPoint)
+  throw new Error('Missing entry point')
+else if (invalidEntryPoint)
+  throw new Error('Invalid entry point')
 
 let child: ChildProcess
 
@@ -25,16 +34,15 @@ const rl = readline.createInterface({
 })
 
 const watchAndBuild = async () => {
+  // TODO: remove `options.entry` on 1.0
+  const entryPoints = options.entry ? [options.entry] : options._
   // This is set to the built library inside node_modules
   const outdir = options.outdir || path.join(__dirname, 'build')
   child && child.kill()
   const service = await esbuild.startService()
   options.clear && console.clear()
-  // TODO: switch to yargs later, as minimist doesn't support array options on 
-  await service.build({
-    entryPoints: [options.entry],
-    outdir
-  })
+  // TODO: switch to yargs later, as minimist doesn't support array options on
+  await service.build({ entryPoints, outdir })
   return new Promise<void>(resolve => {
     if (options.run) {
       rl.pause()
